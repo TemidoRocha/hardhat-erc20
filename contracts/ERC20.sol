dafeec82fd58907c36bfb3bdd34107ad7e6a9fec
TemidoRocha <manueltemidorocha@gmail.com>
2022-08-24 11:33:24 +0100

    first commit

---
+++ contracts/ERC20.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+contract ERC20 {
+  uint256 public totalSupply;
+  string public name;
+  string public symbol;
+  
+  mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowence;
+
+  // storage - state variable stored in blockchain
+  // memory - variable is in memory, exists only while function is being called
+  // calldata - special data location like memory but cheaper
+
+
+  constructor(string memory _name, string memory _symbol){
+    name = _name;
+    symbol = _symbol;
+
+    _mint(msg.sender, 100e18);
+  }
+
+  function decimals() public pure returns (uint8){
+    return 18;
+  }
+
+  function transfer(address recipient, uint256 amount) external returns (bool) {
+    return _transfer(msg.sender, recipient, amount);
+  }
+  
+  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
+    uint256 currentAllowence = allowence[sender][msg.sender];
+    require(
+      currentAllowence >= amount,
+      "ERC20: transfer exceeds allowence"
+    );
+    
+    allowence[sender][msg.sender] = currentAllowence - amount;
+
+    return _transfer(sender, recipient, amount);
+  }
+  
+  function approve(address spender, uint256 amount) external returns(bool) {
+    require(spender != address(0), "ERC20: approve to the zero address");
+
+    allowence[msg.sender][spender] = amount;
+
+    return true;
+  }
+
+  function _transfer(address sender, address recipient, uint256 amount) private returns (bool) {
+    require(recipient !=  address(0), "ERC20: transfer to the zero address");
+
+    uint256 senderBalance = balanceOf[sender];
+
+    require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+
+    balanceOf[sender] = senderBalance - amount;
+    balanceOf[recipient] += amount;
+
+    return true;
+  }
+
+  function _mint(address to, uint256 amount) internal {
+    require(to != address(0), "ERC20: mint to the zero address");
+
+    totalSupply += amount;
+    balanceOf[to] += amount;
+  }
+
+}
---
+++ test/ERC20.ts
@@ -0,0 +1,41 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { ERC20 } from "../typechain-types/ERC20";
+
+
+describe("ERC20Contract", function () {
+  let ERC20Contract: ERC20;
+  let someAddress: SignerWithAddress;
+  let someOtherAddress: SignerWithAddress;
+
+
+  beforeEach(async function(){
+    const ERC20ContractFactory = await ethers.getContractFactory("ERC20");
+    ERC20Contract = await ERC20ContractFactory.deploy("hello", "SYM");
+    await ERC20Contract.deployed();
+
+    someAddress = (await ethers.getSigners())[1];
+    someOtherAddress = (await ethers.getSigners())[2];
+  })
+
+  describe("When I have 10 tokens", function(){
+    beforeEach(async function() {
+      await ERC20Contract.transfer(someAddress.address, 10);
+    })
+    describe("When I transfer 10 tokens", function(){
+      it("should transfer token correctly", async function(){
+        await ERC20Contract.connect(someAddress).transfer(someOtherAddress.address, 10);
+  
+        expect(await ERC20Contract.balanceOf(someOtherAddress.address)).to.equal(10);
+      })
+    })
+    
+    describe("When I transfer 15 tokens", function(){
+      it("should revert the transaction", async function(){
+        await expect(ERC20Contract.connect(someAddress).transfer(someOtherAddress.address, 15)).to.be.revertedWith("ERC20: transfer amount exceeds balance");
+      })
+    })
+  })
+  
+});
